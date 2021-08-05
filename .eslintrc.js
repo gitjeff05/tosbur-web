e9d72a41be4cd38132fe65360244225be6e58638
Jeff Laiosa <jeff@optowealth.com>
2021-08-05 13:53:22 -0400

    config eslint prettier and get reactive state working

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,9 +1,10 @@
+// eslint-disable-next-line no-undef
 module.exports = {
     extends: [
         // add more generic rulesets here, such as:
-        // 'eslint:recommended',
-        'plugin:vue/vue3-recommended',
-        'prettier'
+    'eslint:recommended',
+    'plugin:vue/vue3-recommended',
+    'prettier'
     ],
     rules: {
         // override/add rules settings here, such as:
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,55 @@
 <template>
-  <h1>{{ msg }}</h1>
-  <h2>{{ data.state.dockerVersion.name }}</h2>
-  <p>
-    <a href="https://vitejs.dev/guide/features.html" target="_blank">
-      Vite Documentation
-    </a>
-    |
-    <a href="https://v3.vuejs.org/" target="_blank">Vue 3 Documentation</a>
-  </p>
+    <h1>{{ msg }}</h1>
+    <h2>{{ data.state.dockerVersion.name }}</h2>
+    <p>
+        <a href="https://vitejs.dev/guide/features.html" target="_blank">
+            Vite Documentation
+        </a>
+        |
+        <a href="https://v3.vuejs.org/" target="_blank">Vue 3 Documentation</a>
+    </p>
 
-  <button type="button" @click="handleClick">
-    change docker version {{numOfClicks}}
-  </button>
-
-  <button type="button" @click="getDocker">
-    get docker
-  </button>
-  <p>
-    Edit
-    <code>components/HelloWorld.vue</code> to test hot module replacement.
-  </p>
+    <button type="button" @click="getDocker">get docker</button>
+    <p>
+        Edit
+        <code>components/HelloWorld.vue</code> to test hot module replacement.
+    </p>
 </template>
 
 <script>
-import { ref } from "vue";
-
+import { onMounted, onUpdated } from 'vue';
 export default {
-  props: {
-    msg: String,
-    data: Object
-  },
-  setup(props) {
-    console.log(typeof(props))
-    console.log(Object.keys(props))
-    console.log(props.data)
-    let numOfClicks = ref(0);
-    function handleClick() {
-      numOfClicks.value++;
-    }
-    function getDocker() {
-      props.data.setDockerVersion({ name: 'Jeff' })
-    }
-    return {
-      handleClick,
-      numOfClicks,
-      getDocker
-    };
-  }
+    props: {
+        msg: {
+            type: String,
+            required: true,
+            default: 'Tosbur Web Application',
+        },
+        data: {
+            type: Object,
+            default: () => ({ dockerVersion: { name: 'Jeff' } }),
+            required: false,
+        },
+    },
+    setup(props) {
+        function getDocker() {
+            props.data.setDockerVersion({ name: 'Greg' });
+        }
+        onMounted(() => {
+            console.log('mounted!');
+        });
+        onUpdated(() => {
+            console.log('updated!');
+        });
+        return {
+            getDocker,
+        };
+    },
 };
-
 </script>
 
 <style scoped>
 a {
-  color: #42b983;
+    color: #42b983;
 }
 </style>
